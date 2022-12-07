a00118b405e5d6b8a22e2690958d93734ff66570
Allen <t_araki@yumemi.co.jp>
2022-12-08 01:34:24 +0900

    baseの作成

--- README.md
+++ README.md
@@ -1,35 +1,4 @@
 <!-- @format -->
 
-# eslint-config
-
-## .prettierrc.js
-
-```js
-module.exports = {
-  // default config
-  ...require("@allen-packages/prettier-config"),
-  // orverride parameters
-  semi: false,
-};
-```
-
-## getting started
-
-### npm
-
-`npm install --save-dev prettier @allen-packages/prettier-config`
-
-### yarn1 系
-
-`yarn add --dev eslint prettier @allen-packages/prettier-config`
-
-
-## .prettierignore template
-
-```
-package.json
-package-lock.json
-yarn.lock
-node_modules
-```
 # stylelint-config
+
--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/** @format */
-
 module.exports = {
-  semi: true,
-  printWidth: 100,
-  tabWidth: 2, // インデントのスペースの数を指定
-  useTabs: false, // スペースではなくタブで行をインデントする
-  singleQuote: false, // シングルクオートかダブルクオートか
-  quoteProps: "consistent", // ref https://zenn.dev/ikura1/articles/96e62cc166dba3d64bc8#%E8%A3%9C%E8%B6%B3
-  jsxSingleQuote: false, // jsxの場合 hoge="fuga" にするか hoge='fuga' にするかどうか
-  trailingComma: "all",
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  arrowParens: "always",
-  // 0行目から9000行目までフォーマット
-  rangeStart: 0,
-  rangeEnd: 9000,
-  endOfLine: "lf", // 改行の文字コードを指定
-  embeddedLanguageFormatting: "auto", // Prettierがファイルに埋め込まれた引用コードをフォーマットするかどうかを制御
-
-  // 将来的に細かい設定をしたい時のために一応
-  insertPragma: true,
-  // parser: "none", // ref https://qiita.com/takeshisakuma/items/bbb2cd2f1c65de70e363#parser
-  // filepath: "none",
+  extends: "stylelint-config-standard"
 };
